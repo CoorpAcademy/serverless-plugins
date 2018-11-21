49687677302725b279c1663727216a75ad55af03
Adriean Khisbe <adriean.khisbe@live.fr>
2018-11-21 10:32:26 +0100

    Update offline-kinesis handler to output the kinesis message

--- packages/serverless-offline-kinesis/example/handler.js
+++ packages/serverless-offline-kinesis/example/handler.js
@@ -1,4 +1,13 @@
 module.exports.handler = (event, context) => {
-  console.log(JSON.stringify(event, null, 4));
+  // Print the whole event
+  // console.log(JSON.stringify(event, null, 4));
+
+  // Print only the record data
+  console.log(
+    event.Records &&
+      event.Records.map(record => {
+        return Buffer.from(record.kinesis.data, 'base64').toString('utf8');
+      })
+  );
   return Promise.resolve();
 };
