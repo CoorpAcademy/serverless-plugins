b50ead8a5c3ead70efbfc7d37ae4873db2fe9742
Adriean Khisbe <adriean.khisbe@live.fr>
2019-02-05 17:49:27 +0100

    Prototype the creation of a stage related to the aws deployment the stage hold the AccessLogSetting configuration

--- packages/serverless-apigateway-access-logs/src/index.js
+++ packages/serverless-apigateway-access-logs/src/index.js
@@ -1,29 +1,42 @@
 class ExtendDeploymentWithAccessLogs {
   constructor(serverless, options) {
-    this.serverless = serverless
+    this.serverless = serverless;
+    this.provider = this.serverless.getProvider('aws');
+    console.log(serverless);
     this.hooks = {
       'before:aws:package:finalize:mergeCustomProviderResources': this.bindDeploymentId.bind(this)
-    }
+    };
   }
 
   bindDeploymentId() {
-    const template = this.serverless.service.provider.compiledCloudFormationTemplate
+    const template = this.serverless.service.provider.compiledCloudFormationTemplate;
 
     // Find the deployment resource and patch it
-    for (let key of Object.keys(template.Resources)) {
-      const resource = template.Resources[key]
+    for (const key of Object.keys(template.Resources)) {
+      const resource = template.Resources[key];
       if (resource.Type === 'AWS::ApiGateway::Deployment') {
-        resource.Properties.StageDescription = {
-          AccessLogSetting: {
-            Format: '{ "requestId":"$context.requestId", "ip": "$context.identity.sourceIp", "caller":"$context.identity.caller", "user":"$context.identity.user","requestTime":"$context.requestTime", "httpMethod":"$context.httpMethod","resourcePath":"$context.resourcePath", "status":"$context.status","protocol":"$context.protocol", "responseLength":"$context.responseLength" }',
-            DestinationArn: {
-              'Fn::GetAtt': 'CloudWatchLogsGroup.Arn'
+        const deploymentId = key;
+        resource.Properties.StageName = '__unused_stage__';
+
+        template.Resources.ApiGatewayStage = {
+          Type: 'AWS::ApiGateway::Stage',
+          DependsOn: [deploymentId],
+          Properties: {
+            RestApiId: this.provider.getApiGatewayRestApiId(),
+            StageName: this.provider.getStage(),
+            DeploymentId: {Ref: deploymentId},
+            AccessLogSetting: {
+              Format:
+                '{"apptype":"api-product", "requestId":"$context.requestId", "ip": "$context.identity.sourceIp", "caller":"$context.identity.caller", "user":"$context.identity.user","requestTime":"$context.requestTime", "httpMethod":"$context.httpMethod","resourcePath":"$context.resourcePath", "status":"$context.status","protocol":"$context.protocol", "responseLength":"$context.responseLength" }',
+              DestinationArn: {
+                'Fn::GetAtt': 'CloudWatchLogsGroup.Arn'
+              }
             }
           }
-        }
+        };
       }
     }
   }
 }
 
-module.exports = ExtendDeploymentWithAccessLogs; 
+module.exports = ExtendDeploymentWithAccessLogs;
