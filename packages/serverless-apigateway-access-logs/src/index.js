f08457ac5b6e0fa9a1c755f78c3ec170245c8fc2
Adriean Khisbe <adriean.khisbe@live.fr>
2019-02-06 12:11:11 +0100

    Make the serverless apigateway-access-logs configurable

--- packages/serverless-apigateway-access-logs/src/index.js
+++ packages/serverless-apigateway-access-logs/src/index.js
@@ -1,8 +1,21 @@
+const _ = require('lodash/fp');
+
 class ExtendDeploymentWithAccessLogs {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.provider = this.serverless.getProvider('aws');
-    console.log(serverless);
+
+    this.configuration = _.get('service.custom.serverless-apigateway-access-logs', serverless);
+    if (!this.configuration)
+      throw new Error('Plugin serverless-apigateway-access-logs must be configured');
+    if (!_.has('format', this.configuration))
+      throw new Error(
+        'Plugin serverless-apigateway-access-logs must be configured: missing format'
+      );
+    if (!_.has('log-group', this.configuration))
+      throw new Error(
+        'Plugin serverless-apigateway-access-logs must be configured: missing log-group'
+      );
     this.hooks = {
       'before:aws:package:finalize:mergeCustomProviderResources': this.bindDeploymentId.bind(this)
     };
@@ -12,12 +25,19 @@ class ExtendDeploymentWithAccessLogs {
     const template = this.serverless.service.provider.compiledCloudFormationTemplate;
 
     // Find the deployment resource and patch it
-    for (const key of Object.keys(template.Resources)) {
+    Object.keys(template.Resources).forEach(key => {
       const resource = template.Resources[key];
       if (resource.Type === 'AWS::ApiGateway::Deployment') {
         const deploymentId = key;
         resource.Properties.StageName = '__unused_stage__';
 
+        template.Resources.CloudWatchLogsGroup = {
+          Type: 'AWS::Logs::LogGroup',
+          Properties: {
+            LogGroupName: this.configuration['log-group'],
+            RetentionInDays: this.configuration['log-group-retention'] || 7
+          }
+        };
         template.Resources.ApiGatewayStage = {
           Type: 'AWS::ApiGateway::Stage',
           DependsOn: [deploymentId],
@@ -26,8 +46,7 @@ class ExtendDeploymentWithAccessLogs {
             StageName: this.provider.getStage(),
             DeploymentId: {Ref: deploymentId},
             AccessLogSetting: {
-              Format:
-                '{"apptype":"api-product", "requestId":"$context.requestId", "ip": "$context.identity.sourceIp", "caller":"$context.identity.caller", "user":"$context.identity.user","requestTime":"$context.requestTime", "httpMethod":"$context.httpMethod","resourcePath":"$context.resourcePath", "status":"$context.status","protocol":"$context.protocol", "responseLength":"$context.responseLength" }',
+              Format: this.configuration.format,
               DestinationArn: {
                 'Fn::GetAtt': 'CloudWatchLogsGroup.Arn'
               }
@@ -35,7 +54,7 @@ class ExtendDeploymentWithAccessLogs {
           }
         };
       }
-    }
+    });
   }
 }
 
