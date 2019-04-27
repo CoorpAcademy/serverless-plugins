41c4dabd0f22533ba7554fb5cd00dc16b6b63ff7
godu <arthur.weber@viacesi.fr>
2019-04-27 19:50:51 +0200

    Adds callback tests

--- packages/serverless-offline-sqs/example/handler.js
+++ packages/serverless-offline-sqs/example/handler.js
@@ -1,4 +1,9 @@
-module.exports.handler = (event, context) => {
+module.exports.promise = (event, context) => {
   console.log(JSON.stringify(event, null, 4));
   return Promise.resolve();
+};
+
+module.exports.callback = (event, context, cb) => {
+  console.log(JSON.stringify(event, null, 4));
+  cb();
 };
