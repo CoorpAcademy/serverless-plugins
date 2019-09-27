9bf5e183851646d43bc2c1f063dbe65c0ba22d61
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-09-27 22:33:55 +0200

    Reorganisation having docker-compose only at top level

---
+++ scripts/clean-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+service=$1
+
+function dc () {
+    docker-compose -f docker-compose.test.yml $@
+}
+
+dc stop $service
+dc rm -f $service
+dc stop $service-create
+dc rm -f $service-create
+dc up -d $service-create
+echo "> Service $service was started"
+sidekick_container=$(dc ps --quiet $service-create)
+docker wait $sidekick_container > /dev/null
+echo "> Setup of $service is now complete"
