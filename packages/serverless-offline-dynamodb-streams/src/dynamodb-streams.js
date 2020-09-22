CoorpAcademy/serverless-plugins

commit 3f978d471f4ea203a9f1aa5c543bcbe57d324b84

Wait for (#140)

packages/serverless-offline-dynamodb-streams/src/dynamodb-streams.js
@@ -46,6 +46,19 @@ class DynamodbStreams {
     return this._dynamodbStreamsEvent(functionKey, dynamodbStreamsEvent);
   }
 
+  async _describeTable(tableName) {
+    try {
+      await this.client.waitFor('tableExists', {TableName: tableName}).promise();
+      return await this.client
+        .describeTable({
+          TableName: tableName
+        })
+        .promise();
+    } catch (err) {
+      return this._waitFor(tableName);
+    }
+  }
+
   async _dynamodbStreamsEvent(functionKey, dynamodbStreamsEvent) {
     const {
       enabled,
@@ -60,11 +73,7 @@ class DynamodbStreams {
 
     const {
       Table: {LatestStreamArn}
-    } = await this.client
-      .describeTable({
-        TableName: tableName
-      })
-      .promise();
+    } = await this._describeTable(tableName);
 
     const {
       StreamDescription: {Shards: shards}

packages/serverless-offline-kinesis/src/kinesis.js
@@ -41,6 +41,19 @@ class Kinesis {
     return this._kinesisEvent(functionKey, kinesisEvent);
   }
 
+  async _describeStream(streamName) {
+    try {
+      await this.client.waitFor('streamExists', {StreamName: streamName}).promise();
+      return await this.client
+        .describeStream({
+          StreamName: streamName
+        })
+        .promise();
+    } catch (err) {
+      return this._waitFor(streamName);
+    }
+  }
+
   async _kinesisEvent(functionKey, kinesisEvent) {
     const {enabled, streamName, arn, batchSize, startingPosition} = kinesisEvent;
 
@@ -48,11 +61,7 @@ class Kinesis {
 
     const {
       StreamDescription: {Shards: shards}
-    } = await this.client
-      .describeStream({
-        StreamName: streamName
-      })
-      .promise();
+    } = await this._describeStream(streamName);
 
     shards.forEach(({ShardId: shardId}) => {
       const readable = KinesisReadable(

packages/serverless-offline-s3/src/s3.js
@@ -4,6 +4,8 @@ const {assign, toNumber} = require('lodash/fp');
 const S3EventDefinition = require('./s3-event-definition');
 const S3Event = require('./s3-event');
 
+const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
+
 class S3 {
   constructor(lambda, resources, options) {
     this.lambda = null;
@@ -43,8 +45,17 @@ class S3 {
     return this._s3Event(functionKey, s3Event);
   }
 
-  _s3Event(functionKey, s3Event) {
+  async _waitFor(bucket) {
+    const exists = await this.client.bucketExists(bucket);
+    if (exists) return;
+
+    await delay(1000);
+    return this._waitFor(bucket);
+  }
+
+  async _s3Event(functionKey, s3Event) {
     const {event, bucket} = s3Event;
+    await this._waitFor(bucket);
 
     const listener = this.client.listenBucketNotification(bucket, '*', '*', [event]);
 

packages/serverless-offline-sqs/src/sqs.js
@@ -6,6 +6,8 @@ const {logWarning} = require('serverless-offline/dist/serverlessLog');
 const SQSEventDefinition = require('./sqs-event-definition');
 const SQSEvent = require('./sqs-event');
 
+const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
+
 class SQS {
   constructor(lambda, resources, options) {
     this.lambda = null;
@@ -41,6 +43,15 @@ class SQS {
     );
 
     return this._sqsEvent(functionKey, sqsEvent);
+  }
+
+  async _getQueueUrl(queueName) {
+    try {
+      return await this.client.getQueueUrl({QueueName: queueName}).promise();
+    } catch (err) {
+      await delay(10000);
+      return this._getQueueUrl(queueName);
+    }
   }
 
   async _sqsEvent(functionKey, sqsEvent) {

commit 6f7214ce9d2666aa7332c37befb5562841d77d3e

Fix 'waitFor' recursive calls in kinesis/dynamo-stream :ocean:
Problem apparently introduced by #140 and reported in #145

Might be the cause of instabilities CI is facing currenly  :crossed_fingers:

packages/serverless-offline-dynamodb-streams/src/dynamodb-streams.js
@@ -58,7 +58,7 @@ class DynamodbStreams {
         })
         .promise();
     } catch (err) {
-      return this._waitFor(tableName);
+      return this._describeTable(tableName);
     }
   }
 

packages/serverless-offline-kinesis/src/kinesis.js
@@ -53,7 +53,7 @@ class Kinesis {
         })
         .promise();
     } catch (err) {
-      return this._waitFor(streamName);
+      return this._describeStream(streamName);
     }
   }
 